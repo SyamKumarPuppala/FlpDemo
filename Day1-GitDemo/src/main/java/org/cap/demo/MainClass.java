5ed5225fd5176f6075784ad5d4ce325ddc42e80b
pnayana <pnayana@DIN73002980.corp.capgemini.com>
2018-08-06 14:49:34 +0530

    syam is mad is added

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -7,6 +7,7 @@ public class MainClass {
  public static void greet() {
 	 System.out.println("hii Syam kumar Puppala!!!!!!!");
 	 System.out.println("Hello Syam!!");
+	 System.out.println("Syam is mad!!");
 	 System.out.println("Hello");
 	 AddSales a = new AddSales();
 	 a.show();
